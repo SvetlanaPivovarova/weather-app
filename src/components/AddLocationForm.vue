aca7c4553e2fb829271efe4cf459481d32844df1
svetlana <fatinialux@yandex.ru>
2023-08-07 17:50:44 +0400

    feat: add verification of cities in Settings

--- src/components/AddLocationForm.vue
+++ src/components/AddLocationForm.vue
@@ -11,15 +11,18 @@
       />
       <button
           class="icon icon-58 icon--add button--add"
-          :disabled="isValid"
+          :disabled="!isValid"
           @click="addLocation"
       >
       </button>
     </div>
+    <span class="input-text-label_type_error">{{ errorMessage }}</span>
   </div>
 </template>
 
 <script>
+import {API_KEY, PATTERN_NAME} from "@/utils/constants";
+
 export default {
   name: 'AddLocationForm',
   emits: ['add'],
@@ -27,13 +30,37 @@ export default {
     return {
       newLocation: null,
       locations: [],
-      isValid: false,
+      errorMessage: null
     }
   },
   methods: {
     addLocation() {
-      this.$emit('add', this.newLocation)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.newLocation}&appid=${API_KEY}&units=metric`)
+          .then((response) => response.json())
+          .then((result) => {
+            localStorage.setItem(`${this.newLocation}`, JSON.stringify(result));
+            if(result.message) {
+              this.errorMessage = result.message
+            }
+            else {
+              this.$emit('add', this.newLocation)
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+
+          })
     }
   },
+  computed: {
+    isValid() {
+      return PATTERN_NAME.test(this.newLocation)
+    },
+  },
+  watch: {
+    'newLocation'() {
+      this.errorMessage = null
+    }
+  }
 }
 </script>
