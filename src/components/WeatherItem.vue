a024b158f72ad8a842b77d273114067754dae19b
svetlana <fatinialux@yandex.ru>
2023-08-07 22:53:59 +0400

    feat: handle geolocation

--- src/components/WeatherItem.vue
+++ src/components/WeatherItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!isLoading" class="b-info">
+  <div class="b-info">
     <div class="b-info--section">
       <div class="icon icon-100 icon-weather"></div>
       <div class="b-info--wrapper">
@@ -9,15 +9,13 @@
     <div class="b-info--text">Feels like {{ getFeelsLike }} ℃, {{ getDescription }}</div>
   </div>
   <Info
-      v-if="!isLoading"
+
       :wind="getWind"
       :humidity="getHumidity"
       :pressure="getPressure"
       :visibility="getVisibility"
   />
-  <div v-if="isLoading" class="b-info--loader-container">
-    <Loader />
-  </div>
+
   <Modal :show="showModal" @close="showModal = false" >
     <template #body>
       <p class="b-info--tooltip">{{ error }}</p>
@@ -29,15 +27,11 @@
 <script>
 import Info from "./Info.vue";
 import Modal from "@/components/Modal.vue";
-import {API_KEY} from "../utils/constants";
-import Loader from "./Loader.vue";
 import Settings from "@/components/Settings.vue";
-
-import { locationStorage } from "@/utils/utils";
 
 export default {
   name: 'WeatherItem',
-  components: {Settings, Loader, Info, Modal },
+  components: {Settings, Info, Modal },
   props: {
     weather: {
       type: Object,
@@ -50,16 +44,8 @@ export default {
       error: '',
       isLoading: null,
       showModal: false,
-      locations: locationStorage.fetch(),
+      locations: [],
       refresh: false,
-    }
-  },
-  watch: {
-    'refresh'() {
-      if (this.refresh) {
-        console.log(this.refresh)
-        location.reload()
-      }
     }
   },
   computed: {
@@ -84,54 +70,6 @@ export default {
     getDescription() {
       return this.weather.weather[0].description
     }
-  },
-  mounted() {
-    this.isLoading = true;
-    if(!locationStorage.fetch()) {
-      this.geoFindMe()
-    }
-    else {
-      for (let i = 0; i < this.locations.length; i++) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.locations[i].name}&appid=${API_KEY}&units=metric`)
-            .then((response) => response.json())
-            .then((result) => {
-              localStorage.setItem(`${this.locations[i].name}`, JSON.stringify(result));
-            })
-            .catch((error) => {
-              console.log(error);
-            })
-            .finally(() => this.isLoading = false);
-      }
-    }
-  },
-  methods: {
-    handleSuccess(position) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`)
-          .then((response) => response.json())
-          .then((result) => {
-            this.locations.push({
-              id: locationStorage.uid++,
-              name: result.name,
-            });
-            locationStorage.save(this.locations);
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => this.isLoading = false);
-    },
-    handleError() {
-      this.error = "Unable to get your location. Select below";
-      this.isLoading = false;
-      this.showModal = true;
-    },
-    geoFindMe() {
-      if (!navigator.geolocation) {
-        this.error = "Geolocation не поддерживается вашим браузером";
-      } else {
-        navigator.geolocation.getCurrentPosition(this.handleSuccess, this.handleError);
-      }
-    },
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import './main.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// app.use(createPinia())
 
 app.mount('#app')
